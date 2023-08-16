2b89fec261af6333f07bd80b12a18e2f2bbfb7c0
altruistor <36813847+altruistor@users.noreply.github.com>
2023-08-16 15:42:43 +0200

    homework 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,9 +44,23 @@ class App extends Component {
     }));
   };
 
-  chengeFilter = e => {
+  changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
+
+   componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   render() {
     const { filter } = this.state;
